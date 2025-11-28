8d6d699a60b0647a089467a698f709099b74b86c
Evelyn <eyohanna@asu.edu>
2025-11-27 21:56:35 -0700

    fixed connection

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ from supabase import create_client, Client
 # Initialize connection.
 # Uses st.cache_resource to only run once.
 @st.cache_resource
+@st.cache_resource
 def init_connection():
-    url = st.secrets["SUPABASE_URL"]
-    key = st.secrets["SUPABASE_KEY"]
+    url = st.secrets["connections"]["supabase"]["SUPABASE_URL"]
+    key = st.secrets["connections"]["supabase"]["SUPABASE_KEY"]
     return create_client(url, key)
 
+
+Signup, Login = st.tabs(["Sign Up", "Log In"])
+
 supabase = init_connection()
+
+def login(email, password):
+    user = supabase.auth.sign_in_with_password({"email": email, "password": password})
+    
